9b267c9b85972bfb3bceed6dabdaf3a1e9f8a21d
Loïc <loic.marie@gmail.com>
2013-09-01 22:55:58 +0200

    Ajout des sons + handler sur récupération d'un qr code

---
+++ TestMultiPhotos/src/com/example/testmultiphotos/IMainActivity.java
@@ -0,0 +1,11 @@
+package com.example.testmultiphotos;
+
+public interface IMainActivity {
+
+	void showLongToast(String text);
+
+	void showShortToast(String text);
+
+	void bruitNouveauQRCode();
+
+}
---
+++ TestMultiPhotos/src/com/example/testmultiphotos/QRCodeHandler.java
@@ -0,0 +1,6 @@
+package com.example.testmultiphotos;
+
+public interface QRCodeHandler {
+
+	void onNewQRCodeRead(String qrCode);
+}
